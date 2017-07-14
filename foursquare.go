88ecec99e945f0739a8bca56273ee935d276c571
Adam Balogh <adambalogh@adambalogh.net>
2017-07-14 19:24:36 +0700

    add venue category

--- foursquare.go
+++ foursquare.go
@@ -1,75 +1,86 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 type CheckIn struct {
-    Meta struct {
-        Code        int    `json:"code"`
-        RequestID   string `json:"requestId"`
-        ErrorDetail string `json:errorDetail`
-    } `json:"meta"`
-    Response struct {
-        Recent []struct {
-            ID   string `json:"id"`
-            User struct {
-                ID        string `json:"id"`
-                FirstName string `json:"firstName"`
-                LastName  string `json:"lastName"`
-            } `json:"user"`
-            Venue struct {
-                Name     string `json:"name"`
-                Location struct {
-                    Lat     float64 `json:"lat"`
-                    Lng     float64 `json:"lng"`
-                    City    string  `json:"city"`
-                    Country string  `json:"country"`
-                } `json:"location"`
-            } `json:"venue"`
-        } `json:"recent"`
-    } `json:"response"`
+	Meta struct {
+		Code        int    `json:"code"`
+		RequestID   string `json:"requestId"`
+		ErrorDetail string `json:errorDetail`
+	} `json:"meta"`
+	Response struct {
+		Recent []struct {
+			ID   string `json:"id"`
+			User struct {
+				ID        string `json:"id"`
+				FirstName string `json:"firstName"`
+				LastName  string `json:"lastName"`
+			} `json:"user"`
+			Venue struct {
+				Name     string `json:"name"`
+				Location struct {
+					Lat     float64 `json:"lat"`
+					Lng     float64 `json:"lng"`
+					City    string  `json:"city"`
+					Country string  `json:"country"`
+				} `json:"location"`
+				Categories []struct {
+					ID         string `json:"id"`
+					Name       string `json:"name"`
+					PluralName string `json:"pluralName"`
+					ShortName  string `json:"shortName"`
+					Icon       struct {
+						Prefix string `json:"prefix"`
+						Suffix string `json:"suffix"`
+					} `json:"icon"`
+					Primary bool `json:"primary"`
+				} `json:"categories"`
+			} `json:"venue"`
+		} `json:"recent"`
+	} `json:"response"`
 }
 
 var lastID string
 
 func getCheckins() {
 
-    var checkIn *CheckIn
-    var url = "https://api.foursquare.com/v2/checkins/recent?limit=1&v=20170712"
-    url = fmt.Sprintf(url+"&oauth_token=%s", config.OAuth)
+	var checkIn *CheckIn
+	var url = "https://api.foursquare.com/v2/checkins/recent?limit=1&v=20170712"
+	url = fmt.Sprintf(url+"&oauth_token=%s", config.OAuth)
 
-    resp, err := http.Get(url)
-    checkErr(err)
-    defer resp.Body.Close()
+	resp, err := http.Get(url)
+	checkErr(err)
+	defer resp.Body.Close()
 
-    body, err := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
 
-    if err := json.Unmarshal(body, &checkIn); err != nil {
-        panic(err)
-    }
+	if err := json.Unmarshal(body, &checkIn); err != nil {
+		panic(err)
+	}
 
-    if checkIn.Meta.Code > 200 {
-        fmt.Println(checkIn.Meta.ErrorDetail)
-        return
-    }
+	if checkIn.Meta.Code > 200 {
+		fmt.Println(checkIn.Meta.ErrorDetail)
+		return
+	}
 
-    if len(checkIn.Response.Recent) == 0 {
-        return
-    }
+	if len(checkIn.Response.Recent) == 0 {
+		return
+	}
 
-    if checkIn.Response.Recent[0].ID == lastID {
-        return
-    }
+	if checkIn.Response.Recent[0].ID == lastID {
+		return
+	}
 
-    lastID = checkIn.Response.Recent[0].ID
+	lastID = checkIn.Response.Recent[0].ID
 
-    if len(config.IDs) > 0 && !stringInSlice(checkIn.Response.Recent[0].User.ID, config.IDs) {
-        return
-    }
+	if len(config.IDs) > 0 && !stringInSlice(checkIn.Response.Recent[0].User.ID, config.IDs) {
+		return
+	}
 
-    go postToSlack(checkIn)
+	go postToSlack(checkIn)
 }
--- slack.go
+++ slack.go
@@ -1,81 +1,83 @@
 package main
 
 import (
-    "bytes"
-    "encoding/json"
-    "fmt"
-    "net/http"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
 )
 
 type Field struct {
-    Title string `json:"title,omitempty"`
-    Value string `json:"value,omitempty"`
-    Short bool   `json:"short,omitempty"`
+	Title string `json:"title,omitempty"`
+	Value string `json:"value,omitempty"`
+	Short bool   `json:"short,omitempty"`
 }
 
 type Attachment struct {
-    Fallback   string  `json:"fallback,omitempty"`
-    Color      string  `json:"color,omitempty"`
-    AuthorName string  `json:"author_name,omitempty"`
-    AuthorLink string  `json:"author_link,omitempty"`
-    AuthorIcon string  `json:"author_icon,omitempty"`
-    Title      string  `json:"title,omitempty"`
-    TitleLink  string  `json:"title_link,omitempty"`
-    Pretext    string  `json:"pretext,omitempty"`
-    Fields     []Field `json:"fields,omitempty"`
-    ImageURL   string  `json:"image_url,omitempty"`
-    ThumbURL   string  `json:"thumb_url,omitempty"`
-    FooterIcon string  `json:"footer,omitempty"`
-    Footer     string  `json:"footer_icon,omitempty"`
-    Timestamp  int     `json:"ts,omitempty"`
+	Fallback   string  `json:"fallback,omitempty"`
+	Color      string  `json:"color,omitempty"`
+	AuthorName string  `json:"author_name,omitempty"`
+	AuthorLink string  `json:"author_link,omitempty"`
+	AuthorIcon string  `json:"author_icon,omitempty"`
+	Title      string  `json:"title,omitempty"`
+	TitleLink  string  `json:"title_link,omitempty"`
+	Pretext    string  `json:"pretext,omitempty"`
+	Fields     []Field `json:"fields,omitempty"`
+	ImageURL   string  `json:"image_url,omitempty"`
+	ThumbURL   string  `json:"thumb_url,omitempty"`
+	FooterIcon string  `json:"footer,omitempty"`
+	Footer     string  `json:"footer_icon,omitempty"`
+	Timestamp  int     `json:"ts,omitempty"`
 }
 
 type Message struct {
-    Text        string        `json:"text"`
-    Channel     string        `json:"channel,omitempty"`
-    UserName    string        `json:"username,omitempty"`
-    IconURL     string        `json:"icon_url,omitempty"`
-    IconEmoji   string        `json:"icon_emoji,omitempty"`
-    Attachments []*Attachment `json:"attachments,omitempty"`
+	Text        string        `json:"text"`
+	Channel     string        `json:"channel,omitempty"`
+	UserName    string        `json:"username,omitempty"`
+	IconURL     string        `json:"icon_url,omitempty"`
+	IconEmoji   string        `json:"icon_emoji,omitempty"`
+	Attachments []*Attachment `json:"attachments,omitempty"`
 }
 
 func postToSlack(checkIn *CheckIn) {
 
-    text := fmt.Sprintf("%s %s\n%s\n%s, %s [<http://maps.google.com/?q=%.6f,%.6f|map>]",
-        checkIn.Response.Recent[0].User.FirstName,
-        checkIn.Response.Recent[0].User.LastName,
-        checkIn.Response.Recent[0].Venue.Name,
-        checkIn.Response.Recent[0].Venue.Location.City,
-        checkIn.Response.Recent[0].Venue.Location.Country,
-        checkIn.Response.Recent[0].Venue.Location.Lat,
-        checkIn.Response.Recent[0].Venue.Location.Lng)
+	text := fmt.Sprintf("%s %s\n%s [%s]\n%s, %s [<http://maps.google.com/?q=%.6f,%.6f|map>]",
+		checkIn.Response.Recent[0].User.FirstName,
+		checkIn.Response.Recent[0].User.LastName,
+		checkIn.Response.Recent[0].Venue.Name,
+		checkIn.Response.Recent[0].Venue.Categories[0].Name,
+		checkIn.Response.Recent[0].Venue.Location.City,
+		checkIn.Response.Recent[0].Venue.Location.Country,
+		checkIn.Response.Recent[0].Venue.Location.Lat,
+		checkIn.Response.Recent[0].Venue.Location.Lng)
 
-    fallback := fmt.Sprintf("Check-in: %s %s @ %s (%s, %s) [<http://maps.google.com/?q=%.6f,%.6f|map>]",
-        checkIn.Response.Recent[0].User.FirstName,
-        checkIn.Response.Recent[0].User.LastName,
-        checkIn.Response.Recent[0].Venue.Name,
-        checkIn.Response.Recent[0].Venue.Location.City,
-        checkIn.Response.Recent[0].Venue.Location.Country,
-        checkIn.Response.Recent[0].Venue.Location.Lat,
-        checkIn.Response.Recent[0].Venue.Location.Lng)
+	fallback := fmt.Sprintf("Check-in: %s %s @ %s [%s], (%s, %s) [<http://maps.google.com/?q=%.6f,%.6f|map>]",
+		checkIn.Response.Recent[0].User.FirstName,
+		checkIn.Response.Recent[0].User.LastName,
+		checkIn.Response.Recent[0].Venue.Name,
+		checkIn.Response.Recent[0].Venue.Categories[0].Name,
+		checkIn.Response.Recent[0].Venue.Location.City,
+		checkIn.Response.Recent[0].Venue.Location.Country,
+		checkIn.Response.Recent[0].Venue.Location.Lat,
+		checkIn.Response.Recent[0].Venue.Location.Lng)
 
-    buf, err := json.Marshal(Message{
-        Channel: config.Channel,
-        Attachments: []*Attachment{
-            {
-                Fallback: fallback,
-                Color:    "#FFA500",
-                Fields: []Field{
-                    {
-                        Title: "Check in",
-                        Value: text,
-                        Short: false,
-                    },
-                },
-            },
-        }})
+	buf, err := json.Marshal(Message{
+		Channel: config.Channel,
+		Attachments: []*Attachment{
+			{
+				Fallback: fallback,
+				Color:    "#FFA500",
+				Fields: []Field{
+					{
+						Title: "Check in",
+						Value: text,
+						Short: false,
+					},
+				},
+			},
+		}})
 
-    resp, err := http.Post(config.SlackURL, "application/json", bytes.NewReader(buf))
-    checkErr(err)
-    defer resp.Body.Close()
+	resp, err := http.Post(config.SlackURL, "application/json", bytes.NewReader(buf))
+	checkErr(err)
+	defer resp.Body.Close()
 }
